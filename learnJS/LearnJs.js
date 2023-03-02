9094c541a56b70247e82ff2b9abf9446ba0647d1
Kosta Kazakov <kostakazakoff@gmail.com>
2023-03-02 17:04:59 +0200

    learn JS

--- learnJS/LearnJs.js
+++ learnJS/LearnJs.js
@@ -1,17 +1,22 @@
-let countEl = document.getElementById('counter-text');
-let infoEl = document.getElementById('saved-info')
 let counter = 0;
 let countArr = [];
+const countEl = document.getElementById('counter-text');
+const infoEl = document.getElementById('saved-info')
+const countBtn = document.getElementById("count-btn");
+const saveBtn = document.getElementById("save-btn");
+const submitBtn = document.getElementById("submit-btn")
+const resetBtn = document.getElementById("reset-btn")
 
-function increment() {
+countBtn.addEventListener('click', function () {
     counter ++;
     countEl.textContent = counter;
     console.log(counter);
-}
+})
 
-function saveInfo() {
+saveBtn.addEventListener('click', function () {
     countArr.push(counter)
     counter = 0;
     countEl.textContent = counter;
     infoEl.value = countArr.join(' ')
-}
+})
+
